a9b0c4be84c7957944365dcad7b89739fedf2076
ariamanggala <a22100022@mghs.stmik-sumedang.ac,id>
2023-04-18 14:06:33 +0700

    update file database

--- kuliah/database/mahasiswa.sql
+++ kuliah/database/mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2023 at 06:21 AM
+-- Generation Time: Apr 18, 2023 at 02:05 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -29,13 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mahasiswa` (
   `id` int(10) NOT NULL,
-  `nim` int(10) NOT NULL,
-  `nama_mhs` varchar(60) NOT NULL,
-  `foto_mhs` blob NOT NULL,
-  `jurusan` varchar(20) NOT NULL,
-  `semester` varchar(5) NOT NULL,
-  `email` varchar(50) NOT NULL
+  `nim` varchar(20) NOT NULL,
+  `nama_mhs` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `semester` int(5) NOT NULL,
+  `foto_mhs` varchar(100) NOT NULL,
+  `email` varchar(70) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama_mhs`, `jurusan`, `semester`, `foto_mhs`, `email`) VALUES
+(1, 'A22100001', 'Agus', 'Teknik Informatika', 4, 'foto (1).jpg', 'agus@gmail.com'),
+(2, 'A22100001', 'Eneng', 'Teknik Informatika', 4, 'foto (2).jpg', 'eneng@gmail.com'),
+(3, 'A22100004', 'budi', 'Teknik Informatika', 4, 'foto (5).jpg', 'budi@gmail.com'),
+(4, 'A22100022', 'Aria', 'Teknik Informatika', 4, 'foto (4).jpg', 'manggalaaria2505@gmail.com'),
+(5, 'a2.2100022', 'Aria', 'Teknik Informatika', 4, 'foto (4).jpg', 'manggalaaria2505@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -46,6 +57,16 @@ CREATE TABLE `mahasiswa` (
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
